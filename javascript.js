bed34e1b71f0f650bf7b5b3285511a27ec245086
Oscar Robertson <86749444+orthesavage@users.noreply.github.com>
2023-01-10 22:19:43 +1300

    Handle reset button

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-const squaresContainer = document.querySelector("#squaresContainer");
-
 function createDivs(n) {
     for (i = 0; i < n; i++ ) {
         const row = document.createElement('div');
@@ -18,8 +16,20 @@ function createDivs(n) {
     }
 }
 
+function resetButtonHandler() {
+    while (squaresContainer.hasChildNodes()) {
+        squaresContainer.removeChild(squaresContainer.firstElementChild);
+    }
+
+    createDivs(16);
+}
+
 function hoverHandler(e) {
     e.target.style.backgroundColor = 'White';
 }
+
+const squaresContainer = document.querySelector("#squaresContainer");
+const resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener('click', resetButtonHandler);
 
 createDivs(16);
